df62e701d8efa207935ca3fab6e09d3ac324708b
Gabriel <gabriel.i.73290@gmail.com>
2025-04-01 15:15:04 +0200

    index-redo initial commit

---
+++ public/script/index_old.js
@@ -0,0 +1,20 @@
+let evenement = document.getElementById("boutton4")
+let prestataire = document.getElementById("boutton5")
+
+
+evenement.addEventListener("click", lanceEvenement)
+
+function lanceEvenement()
+{
+    window.location.href = window.location.pathname + 'event'
+}
+
+document.addEventListener("scroll", function() {
+    let sections = document.querySelectorAll(".section");
+    sections.forEach(section => {
+        let rect = section.getBoundingClientRect();
+        if (rect.top < window.innerHeight && rect.bottom > 0) {
+            section.style.opacity = 1;
+        }
+    });
+});
