9c1692985351a5379ffbecb9700bbf407b4dff0c
Gabriel <gabriel.i.73290@gmail.com>
2025-04-15 10:00:36 +0200

    Index v2 terminé

---
+++ public/script/index.js
@@ -0,0 +1,25 @@
+let evenement = document.getElementById("button4")
+let prestataire = document.getElementById("button5")
+
+evenement.addEventListener("click", lanceEvenement)
+prestataire.addEventListener("click", lancePrestataire)
+
+function lanceEvenement()
+{
+    window.location.href = window.location.pathname + 'event'
+}
+
+function lancePrestataire()
+{
+    window.location.href = window.location.pathname + 'admin'
+}
+
+document.addEventListener("scroll", function() {
+    let sections = document.querySelectorAll(".section");
+    sections.forEach(section => {
+        let rect = section.getBoundingClientRect();
+        if (rect.top < window.innerHeight && rect.bottom > 0) {
+            section.style.opacity = 1;
+        }
+    });
+});
